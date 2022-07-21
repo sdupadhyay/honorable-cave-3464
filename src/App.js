fd4976641050d0426ba71c25e836b9b8f9a3dddc
sdupadhyay <104014308+sdupadhyay@users.noreply.github.com>
2022-07-21 11:37:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+// import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Body from "./Components/Body";
+import Newarrivals from "./Components/Newarrivals";
+import Newitems from "./Components/Newitems";
+function App() {
+  let data = [
+    {
+      img: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/4/c/4ca14ce8906087779124_7_.jpg",
+      heading: "Mamaearth Rice Face Scrub For Glowing With Rice Water",
+      price: "350",
+      price1: "304",
+      d: "13% off",
+    },
+    {
+      img: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/i/m/image1_600x.jpg",
+      heading: "Lotus Herbals Whiteglow Vitamin C Radiance Cream Spf 20",
+      price: "475",
+      price1: "428",
+      d: "10% off",
+    },
+    {
+      img: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/3/6/3699c478906105612174-2.jpg",
+      heading: "WOW Skin Caffeine Face Serum with Carcoal and Lemon",
+      price: "599",
+      price1: "515",
+      d: "14% off",
+    },
+    {
+      img: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/l/flowers-of-youth-essence-serum_2_1.jpg",
+      heading: "Mamaearth Flowers of Youth Essence Serum With Hyaluronic Acid",
+      price: "699",
+      price1: "599",
+      d: "10% off",
+    },
+  ];
+  return (
+    <div className="App">
+      <Navbar />
+      <Body />
+      <Newarrivals />
+      <div style={{ display: "flex", width: "93%", margin: "auto" }}>
+        {data.map((ele) => (
+          <Newitems
+            img={ele.img}
+            heading={ele.heading}
+            price={ele.price}
+            price1={ele.price1}
+            d={ele.d}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Newarrivals.jsx
@@ -0,0 +1,23 @@
+export default function newarrivals({ img }) {
+  return (
+    <>
+      <div id="newarrivals">
+        <p>NEW ARRIVALS!</p>
+        {/* <div id="pdata">
+          <div id="nbox">
+            <img className="productimage" src={img} alt="na" />
+            <p>Mamaearth Rice Face Scrub For Glowing With Rice Water</p>
+            <p>⭐⭐⭐⭐⭐(1)</p>
+            <div>
+              <h3 className="originalprice">₹350</h3>
+              <h3 className="offerprice">₹304</h3>
+              <h3 className="discount">13%</h3>
+            </div>
+            <button className="addtocart">Add To Cart</button>
+            <button className="witlist">🤍</button>
+          </div>
+        </div> */}
+      </div>
+    </>
+  );
+}
