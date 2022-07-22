24e20dc1b1b5f40fa8af7fe63b5f0aec3e549036
sdupadhyay <104014308+sdupadhyay@users.noreply.github.com>
2022-07-22 10:23:52 -0400

    Add files via upload

---
+++ Day4/src/App.js
@@ -0,0 +1,13 @@
+// import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import AllRoutes from "./Components/AllRoutes";
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
